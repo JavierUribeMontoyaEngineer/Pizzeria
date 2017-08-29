bc0adbe04edfd671dbd64ba8312aabcf225c5f59
JavierUribeMontoyaEngineer <javier.uribe@coitimur.org>
2017-08-29 20:01:40 +0200

    Proyecto de Concepto para desplegar una API REST con CORS

---
+++ PruebaConceptoAutenticacion/API_REST/Controllers/PizzasController.cs
@@ -0,0 +1,63 @@
+﻿using API_REST.Models;
+using PruebaConceptoAutenticacion.Cors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_REST.Controllers
+{
+    [AllowCrossSite]
+    public class PizzasController : ApiController
+    {
+        //TODO eliminar este metodo y usar el de la BD real
+        private List<Pizza> getPizzasBD()
+        {
+            var pizzas = new List<Pizza>()
+            {
+                new Pizza()
+                {
+                    Id = 1,
+                    Name = "Margarita"
+
+                },
+                new Pizza()
+                {
+                    Id = 2,
+                    Name = "Napolitana"
+                }
+            };
+            return pizzas;
+        }
+        
+        // GET api/pizzas
+        public IEnumerable<Pizza> Get()
+        {        
+            return getPizzasBD();
+        }
+
+        // GET api/pizzas/2
+        public Pizza Get(int id)
+        {
+            var pizzas = getPizzasBD().Where(p => p.Id == id);
+            return pizzas.ToList<Pizza>()[0];
+        }
+
+        // POST api/pizzas
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/pizzas/2
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/pizzas/2
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- PruebaConceptoAutenticacion/PruebaConceptoAutenticacion/Controllers/HomeController.cs
+++ PruebaConceptoAutenticacion/PruebaConceptoAutenticacion/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PruebaConceptoAutenticacion.Cors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
