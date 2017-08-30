b83193323da4923969159687abbbbdf0da7afcc8
Javier Uribe <Usuario@DESKTOP-612>
2017-08-30 10:21:04 +0200

    Añadido boton login con la librería JS de Facebook. Falta guardar la identidad de ese usuario logueado y redirigir al login

---
+++ PruebaConceptoAutenticacion/ApplicationFrontend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApplicationFrontend.MvcApplication" Language="C#" %>
---
+++ PruebaConceptoAutenticacion/ApplicationFrontend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApplicationFrontend.Startup))]
+namespace ApplicationFrontend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
