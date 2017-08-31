7aa6401b14d5f5f14b1fe000bd7b1cfc9273cd9d
JavierUribeMontoyaEngineer <javier.uribe@coitimur.org>
2017-08-31 16:30:17 +0200

    Creada estructura backend

---
+++ PruebaConceptoAutenticacion/Infraestructures/UOW/PizzaContext.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas{ get; set; }
+    }
+}
