2bb61d284c8b424b32d4efbb3e4faf6f6d672159
JavierUribeMontoyaEngineer <javier.uribe@coitimur.org>
2017-09-04 17:43:32 +0200

    Autofac instalado

--- PizzeriaShop/REST/Global.asax.cs
+++ PizzeriaShop/REST/Global.asax.cs
@@ -20,7 +20,23 @@ namespace REST
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             //TODO AUTOFAC
+            var builder = new ContainerBuilder();
 
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            // OPTIONAL: Register the Autofac model binder provider.
+            builder.RegisterWebApiModelBinderProvider();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
     }
 }
