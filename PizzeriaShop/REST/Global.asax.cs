1f74d8b38e405a04cc964aaf815af2c6e04b90e2
JavierUribeMontoyaEngineer <javier.uribe@coitimur.org>
2017-09-04 18:04:26 +0200

    Instalado Autofac WebApi 2

--- PizzeriaShop/REST/Global.asax.cs
+++ PizzeriaShop/REST/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -19,7 +22,6 @@ namespace REST
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            //TODO AUTOFAC
             var builder = new ContainerBuilder();
 
             // Get your HttpConfiguration.
@@ -37,6 +39,9 @@ namespace REST
             // Set the dependency resolver to be Autofac.
             var container = builder.Build();
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+
+
         }
     }
 }
